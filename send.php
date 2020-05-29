af7a11845200dc2cc9cff3bf9b77cd275df90269
ЭМИИА <vstarostin@emiia.ru>
2020-05-29 21:15:49 +0300

    Update send.php

--- send.php
+++ send.php
@@ -2,46 +2,69 @@
 // Файлы phpmailer
 require 'PHPMailer.php';
 require 'SMTP.php';
+require 'Exception.php';
 
+// Переменные, которые отправляет пользователь
 $name = $_POST['name'];
-$number = $_POST['number'];
 $email = $_POST['email'];
+$text = $_POST['text'];
+$file = $_FILES['myfile'];
 
-// Настройки
-$mail = new PHPMailer;
+// Формирование самого письма
+$title = "Заголовок письма";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Почта:</b> $email<br><br>
+<b>Сообщение:</b><br>$text
+";
 
-$mail->isSMTP(); 
-$mail->Host = 'smtp.yandex.ru';  
-$mail->SMTPAuth = true;                      
-$mail->Username = 'emonocle'; // Ваш логин в Яндексе. Именно логин, без @yandex.ru
-$mail->Password = 'dollwlzumtjwdxgp'; // Ваш пароль
-$mail->SMTPSecure = 'ssl';                            
-$mail->Port = 465;
-$mail->setFrom('emonocle@yandex.ru'); // Ваш Email
-$mail->addAddress('example@mail.ru'); // Email получателя
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'emonocle'; // Логин на почте
+    $mail->Password   = 'dollwlzumtjwdxgp'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('emonocle@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
 
-// Прикрепление файлов
-  for ($ct = 0; $ct < count($_FILES['userfile']['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['userfile']['name'][$ct]));
-        $filename = $_FILES['userfile']['name'][$ct];
-        if (move_uploaded_file($_FILES['userfile']['tmp_name'][$ct], $uploadfile)) {
+    // Получатель письма
+    $mail->addAddress('vstarostin@yandex.ru');  
+
+    // Прикрипление файлов к письму
+if (!empty($file['name'][0])) {
+    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+        $filename = $file['name'][$ct];
+        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
             $mail->addAttachment($uploadfile, $filename);
+            $rfile[] = "Файл $filename прикреплён";
         } else {
-            $msg .= 'Failed to move file to ' . $uploadfile;
+            $rfile[] = "Не удалось прикрепить файл $filename";
         }
     }   
-                                 
-// Письмо
-$mail->isHTML(true); 
-$mail->Subject = "Заголовок"; // Заголовок письма
-$mail->Body    = "Имя $name . Телефон $number . Почта $email"; // Текст письма
-
-// Результат
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'ok';
 }
-?>
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
