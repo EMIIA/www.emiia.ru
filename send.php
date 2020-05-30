fd64dbfba6d2af46b8d7702dc050a4ca7f3d0393
ЭМИИА <vstarostin@emiia.ru>
2020-05-30 12:43:01 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,60 +1,43 @@
+<!-- обработка почты необходим phpmailer-->
 <?php
-//----Скрипт отправки почты через SMTP с использованием PHPMailer----
-//Импорт классов PHPMailer в глобальное пространство имен. Они должны быть в верхней части скрипта, а не внутри функции
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-if (!empty($_POST["contact-button"])) {
-$name = $_POST["contact-name"];
-$name = check_symbol($name, "Имя", "1", "/^[A-ZА-ЯЁ]+\z/iu");
-$email = $_POST["contact-email"];
-$email = check_symbol($email, "E-mail", "1", "/^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,6}\z/i");
-$subject = $_POST["contact-subject"];
-$subject = check_symbol($subject, "Тема сообщения", "1", "0");
-$comment = $_POST["contact-comment"];
-$comment = check_symbol($comment, "Текст сообщения", "1", "0");
-if (!empty($GLOBALS['alert'])) {
-$alert = 'Данные из формы не отправлены. Обнаружены следующие ошибки: \n'.$alert;
-include "alert.php";
+require 'class.phpmailer.php';
+
+$cname = htmlspecialchars(strip_tags(trim($_POST['cname'])));
+$cphone = htmlspecialchars(strip_tags(trim($_POST['ctel'])));
+$ctip = htmlspecialchars(strip_tags(trim($_POST['ctip'])));
+
+if($ctip=="call"){    
+        $subject = 'Перезвоните мне.';     
+        $message = 'Просьба о звонке от <b>' . $cname . '</b><br/>';
+        $message .= 'Телефон: <b>' . $cphone . '</b><br/>';
 }
-else {
-//Подключение библиотеки
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/SMTP.php';
-$mail = new PHPMailer(); //Инициализация класса
-$from = 'feedback@avtobezugona.ru'; //Адрес почты, с которой идет отправка письма
-$to = 'admin@avtobezugona.ru'; //Адрес получателя
-$mail -> isSMTP(); //Применение протокола SMTP
-$mail -> Host = 'smtp.yandex.ru';//Адрес почтового сервера
-$mail -> SMTPAuth = true; //Включение режима авторизации
-$mail -> Username = 'emon'; //Логин от доменной почты, подключенной к стороннему почтовому сервису (в данном случае в Яндекс.Почта)
-$mail -> Password = 'doll'; //Пароль от доменной почты
-$mail -> SMTPSecure = 'ssl'; //Протокол шифрования
-$mail -> Port = '465'; //Порт сервера SMTP
-$mail -> CharSet = 'UTF-8'; //Кодировка
-$mail -> setFrom($from, 'emon@yandex.ru'); //Адрес и имя отправителя
-$mail -> addAddress($to, 'vsta@monocle.ru'); //Адрес и имя получателя
-$mail -> isHTML (true); //Установка формата электронной почты в HTML
-$mail -> Subject = 'Отправлена форма обратной связи'; //Тема письма (заголовок)
-$mail -> Body = "
-<html>
-<body>
-<p>Имя отправителя: $name</p>
-<p>Адрес отправителя: $email</p>
-<p>Тема сообщения: $subject</p>
-<p>Содержание сообщения: $comment</p>
-</body>
-</html>
-"; //Содержимое письма
-$mail -> AltBody = 'Текст альтернативного письма'; //Альтернативное письмо в случае, если почтовый клиент не поддерживает формат HTML
-$mail -> SMTPDebug = 1; //Включение отладки SMTP: 0 - выкл (для штатного использования), 1 = сообщения клиента, 2 - сообщения клиента и сервера
-if ($mail -> send()) {
-$alert = 'Сообщение отправлено'; //Вывод сообщения в диалоговом окне браузера об успешной отправке письма
+
+if($ctip=="order"){ 
+        $subject = 'Новая заявка';
+        $message = 'Новая заявка от <b>' . $cname . '</b><br/>';
+        $message .= 'Телефон: <b>' . $cphone . '</b><br/>';
 }
-else {
-$alert = 'Ошибка, письмо не может быть отправлено: '.$mail -> ErrorInfo; //Вывод сообщения об ошибке
-}
-include "send.php";
-}
-}
+
+
+$mail = new PHPMailer; 
+        $mail->CharSet = 'utf-8';
+        $mail->From = 'web@penbrain.ru';      // от кого 
+        $mail->FromName = 'Andrew';   // от кого 
+        $mail->AddAddress('andreiduffy@gmail.com'); // кому - адрес, Имя 
+        $mail->IsHTML(true);        // выставляем формат письма HTML 
+        $mail->Subject = $subject;  // тема письма 
+        $mail->Body = $message; 
+        if(isset($_FILES['attachfile'])) { 
+                if($_FILES['attachfile']['error'] == 0){ 
+                        $mail->AddAttachment($_FILES['attachfile']['tmp_name'], $_FILES['attachfile']['name']); 
+                } 
+         } 
+
+        // отправляем наше письмо 
+        if(!$mail->send()) {
+            echo 0;
+        } else {
+            echo 1;
+        }
 ?>
+<!-- обработка почты -->
