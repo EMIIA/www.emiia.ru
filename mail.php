28cf6b5bcc876eb11c4802504ca837b6b1a6d77c
ЭМИИА <vstarostin@emiia.ru>
2020-07-06 15:05:25 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'emiia.ai@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Yt2OOUt1ryi&'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Password = 'iffleksydbbzqjtv'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
