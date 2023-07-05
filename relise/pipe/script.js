033038a9ecec6803a7b1277ca5f3c7cc9576b8bf
ЭМИИА ИИ <vstarostin@emiia.ru>
2023-07-05 19:07:49 +0300

    Add files via upload

--- relise/pipe/script.js
+++ relise/pipe/script.js
@@ -1,99 +1,191 @@
-const sampleImageUrl = 'http://athenaposters.ca/wp-content/uploads/2017/09/RP15304-Real-Madrid-C-Ronaldo-16.jpg';
-const crossOriginUrl = 'https://cors-anywhere.herokuapp.com/';
-
-const imageScaleFactor = 0.5;
-const outputStride = 16;
-const flipHorizontal = false;
-
-const imageWidth = 500;
-const imageHeight = 500;
-
-let joins = [
-    [0, 1],
-    [0, 2],
-    [1, 3],
-    [2, 4],
-    [5, 6],
-    [5, 7],
-    [6, 8],
-    [7, 9],
-    [8, 10],
-    [5, 11],
-    [6, 12],
-    [11, 12],
-    [11, 13],
-    [12, 14],
-    [13, 15],
-    [14, 16]
-];
-
-async function estimatePoseOnImage(imageElement) {
-    return posenet.load().then((net) => {
-        return net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride)
-    });
-}
-
-function hasStrongProbability(point) {
-    return point.score < 0.65 ? false : true;
-}
-
-function drawEstimatedPoseOnCanvas(context, pose) {
-    context.fillStyle = "#ffe500";
-    context.strokeStyle = "#ffe500";
-
-    points = pose.keypoints;
-
-    points.forEach((point, index, array) => {
-        if (hasStrongProbability(point)) {
-            context.beginPath();
-            context.arc(point.position.x, point.position.y, 2, 0, 2 * Math.PI);
-            context.fill();
+import DeviceDetector from "https://cdn.skypack.dev/device-detector-js@2.2.10";
+// Usage: testSupport({client?: string, os?: string}[])
+// Client and os are regular expressions.
+// See: https://cdn.jsdelivr.net/npm/device-detector-js@2.2.10/README.md for
+// legal values for client and os
+testSupport([
+    { client: 'Chrome' },
+]);
+function testSupport(supportedDevices) {
+    const deviceDetector = new DeviceDetector();
+    const detectedDevice = deviceDetector.parse(navigator.userAgent);
+    let isSupported = false;
+    for (const device of supportedDevices) {
+        if (device.client !== undefined) {
+            const re = new RegExp(`^${device.client}$`);
+            if (!re.test(detectedDevice.client.name)) {
+                continue;
+            }
         }
-    });
-
-    joins.forEach(function(join, index, array) {
-        if (hasStrongProbability(points[join[0]]) && hasStrongProbability(points[join[1]])) {
-            context.moveTo(
-            points[join[0]].position.x,
-            points[join[0]].position.y
-            );
-            context.lineTo(
-            points[join[1]].position.x,
-            points[join[1]].position.y
-            );
-            context.stroke();
+        if (device.os !== undefined) {
+            const re = new RegExp(`^${device.os}$`);
+            if (!re.test(detectedDevice.os.name)) {
+                continue;
+            }
         }
-    });
+        isSupported = true;
+        break;
+    }
+    if (!isSupported) {
+        alert(`This demo, running on ${detectedDevice.client.name}/${detectedDevice.os.name}, ` +
+            `is not well supported at this time, expect some flakiness while we improve our code.`);
+    }
 }
-
-window.onload = () => {
-    let imageUrl = crossOriginUrl + sampleImageUrl;
-    let image = new Image();
-    image.crossOrigin = 'anonymous';
-    image.height = imageWidth;
-    image.width = imageHeight;
-    
-    let canvas = document.getElementById('output_canvas');
-    let imageUrlTextInput = document.getElementById('image_url');
-    let estimatePoseButton = document.getElementById('estimate_pose');
-
-    let context = canvas.getContext('2d');
-
-    estimatePoseButton.addEventListener('click', (event) => {
-        console.log('Starting fetch...');
-        let imageUrl = crossOriginUrl + imageUrlTextInput.value;
-        image.src = imageUrl;
-    });
-
-    image.addEventListener('load', (event) => {
-        console.log('Image fetched!');
-        context.clearRect(0, 0, imageWidth, imageHeight);
-        context.drawImage(image, 0, 0, imageWidth, imageWidth);
-        estimatePoseOnImage(canvas).then((pose) => {
-            console.log(pose);
-            drawEstimatedPoseOnCanvas(context, pose);
-        });
-    });
-
-    image.src = imageUrl;
+const controls = window;
+const LandmarkGrid = window.LandmarkGrid;
+const drawingUtils = window;
+const mpPose = window;
+const options = {
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@${mpPose.VERSION}/${file}`;
+    }
+};
+// Our input frames will come from here.
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const controlsElement = document.getElementsByClassName('control-panel')[0];
+const canvasCtx = canvasElement.getContext('2d');
+// We'll add this to our control panel later, but we'll save it here so we can
+// call tick() each time the graph runs.
+const fpsControl = new controls.FPS();
+// Optimization: Turn off animated spinner after its hiding animation is done.
+const spinner = document.querySelector('.loading');
+spinner.ontransitionend = () => {
+    spinner.style.display = 'none';
+};
+const landmarkContainer = document.getElementsByClassName('landmark-grid-container')[0];
+const grid = new LandmarkGrid(landmarkContainer, {
+    connectionColor: 0xCCCCCC,
+    definedColors: [{ name: 'LEFT', value: 0xffa500 }, { name: 'RIGHT', value: 0x00ffff }],
+    range: 2,
+    fitToGrid: true,
+    labelSuffix: 'm',
+    landmarkSize: 2,
+    numCellsPerAxis: 4,
+    showHidden: false,
+    centered: true,
+});
+let activeEffect = 'mask';
+function onResults(results) {
+    // Hide the spinner.
+    document.body.classList.add('loaded');
+    // Update the frame rate.
+    fpsControl.tick();
+    // Draw the overlays.
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    if (results.segmentationMask) {
+        canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
+        // Only overwrite existing pixels.
+        if (activeEffect === 'mask' || activeEffect === 'both') {
+            canvasCtx.globalCompositeOperation = 'source-in';
+            // This can be a color or a texture or whatever...
+            canvasCtx.fillStyle = '#00FF007F';
+            canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+        }
+        else {
+            canvasCtx.globalCompositeOperation = 'source-out';
+            canvasCtx.fillStyle = '#0000FF7F';
+            canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+        }
+        // Only overwrite missing pixels.
+        canvasCtx.globalCompositeOperation = 'destination-atop';
+        canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+        canvasCtx.globalCompositeOperation = 'source-over';
+    }
+    else {
+        canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+    }
+    if (results.poseLandmarks) {
+        drawingUtils.drawConnectors(canvasCtx, results.poseLandmarks, mpPose.POSE_CONNECTIONS, { visibilityMin: 0.65, color: 'white' });
+        drawingUtils.drawLandmarks(canvasCtx, Object.values(mpPose.POSE_LANDMARKS_LEFT)
+            .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'rgb(255,138,0)' });
+        drawingUtils.drawLandmarks(canvasCtx, Object.values(mpPose.POSE_LANDMARKS_RIGHT)
+            .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'rgb(0,217,231)' });
+        drawingUtils.drawLandmarks(canvasCtx, Object.values(mpPose.POSE_LANDMARKS_NEUTRAL)
+            .map(index => results.poseLandmarks[index]), { visibilityMin: 0.65, color: 'white', fillColor: 'white' });
+    }
+    canvasCtx.restore();
+    if (results.poseWorldLandmarks) {
+        grid.updateLandmarks(results.poseWorldLandmarks, mpPose.POSE_CONNECTIONS, [
+            { list: Object.values(mpPose.POSE_LANDMARKS_LEFT), color: 'LEFT' },
+            { list: Object.values(mpPose.POSE_LANDMARKS_RIGHT), color: 'RIGHT' },
+        ]);
+    }
+    else {
+        grid.updateLandmarks([]);
+    }
 }
+const pose = new mpPose.Pose(options);
+pose.onResults(onResults);
+// Present a control panel through which the user can manipulate the solution
+// options.
+new controls
+    .ControlPanel(controlsElement, {
+    selfieMode: true,
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    enableSegmentation: false,
+    smoothSegmentation: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5,
+    effect: 'background',
+})
+    .add([
+    new controls.StaticText({ title: 'MediaPipe Pose' }),
+    fpsControl,
+    new controls.Toggle({ title: 'Selfie Mode', field: 'selfieMode' }),
+    new controls.SourcePicker({
+        onSourceChanged: () => {
+            // Resets because this model gives better results when reset between
+            // source changes.
+            pose.reset();
+        },
+        onFrame: async (input, size) => {
+            const aspect = size.height / size.width;
+            let width, height;
+            if (window.innerWidth > window.innerHeight) {
+                height = window.innerHeight;
+                width = height / aspect;
+            }
+            else {
+                width = window.innerWidth;
+                height = width * aspect;
+            }
+            canvasElement.width = width;
+            canvasElement.height = height;
+            await pose.send({ image: input });
+        },
+    }),
+    new controls.Slider({
+        title: 'Model Complexity',
+        field: 'modelComplexity',
+        discrete: ['Lite', 'Full', 'Heavy'],
+    }),
+    new controls.Toggle({ title: 'Smooth Landmarks', field: 'smoothLandmarks' }),
+    new controls.Toggle({ title: 'Enable Segmentation', field: 'enableSegmentation' }),
+    new controls.Toggle({ title: 'Smooth Segmentation', field: 'smoothSegmentation' }),
+    new controls.Slider({
+        title: 'Min Detection Confidence',
+        field: 'minDetectionConfidence',
+        range: [0, 1],
+        step: 0.01
+    }),
+    new controls.Slider({
+        title: 'Min Tracking Confidence',
+        field: 'minTrackingConfidence',
+        range: [0, 1],
+        step: 0.01
+    }),
+    new controls.Slider({
+        title: 'Effect',
+        field: 'effect',
+        discrete: { 'background': 'Background', 'mask': 'Foreground' },
+    }),
+])
+    .on(x => {
+    const options = x;
+    videoElement.classList.toggle('selfie', options.selfieMode);
+    activeEffect = x['effect'];
+    pose.setOptions(options);
+});
